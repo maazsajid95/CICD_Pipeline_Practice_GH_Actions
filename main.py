53cf275adaf3100e7be7c2be813cc841ae0837e6
Muaz Sajid <muazsajid@Muazs-Laptop.local>
2026-05-24 12:56:22 -0500

    Initial Flask app

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "CI/CD Pipeline is Active!!"
+
+#todo: later make the ip and port number into env variables
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001)
